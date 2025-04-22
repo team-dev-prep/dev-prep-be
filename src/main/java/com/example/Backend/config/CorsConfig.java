5c508cb15434aae9cdb9264a8afed09b9d7ca233
sonsuchang <son336208@naver.com>
2025-04-22 19:44:10 +0900

    refactor : Frontend 주소 변경

--- src/main/java/com/example/Backend/config/CorsConfig.java
+++ src/main/java/com/example/Backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173")); // React dev server
+        config.setAllowedOriginPatterns(List.of("https://devprep-official.vercel.app/")); // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
