f20205ff18186965b9c90f52052410a4ec10375a
sonsuchang <son336208@naver.com>
2025-05-12 15:56:30 +0900

    refactor : Query 수정

--- src/main/java/com/example/Backend/question/QuestionRepository.java
+++ src/main/java/com/example/Backend/question/QuestionRepository.java
@@ -8,11 +8,15 @@ import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    //TODO 2차목표 진행 시 값을 랜덤하게 가져올 수 있도록 수정
-
-    @Query("SELECT q FROM Question q WHERE q.jobId = :jobId AND q.personality = 1 ORDER BY RAND() LIMIT :count")
+    @Query(
+            value = "SELECT * FROM question WHERE job_id = :jobId AND personality = 1 ORDER BY RAND() LIMIT :count",
+            nativeQuery = true
+    )
     List<Question> findPersonalityQuestions(@Param("jobId") Long jobId, @Param("count") int count);
 
-    @Query("SELECT q FROM Question q WHERE q.jobId = :jobId AND q.personality = 0 ORDER BY RAND() LIMIT :count")
+    @Query(
+            value = "SELECT * FROM question WHERE job_id = :jobId AND personality = 0 ORDER BY RAND() LIMIT :count",
+            nativeQuery = true
+    )
     List<Question> findTechQuestions(@Param("jobId") Long jobId, @Param("count") int count);
 }
